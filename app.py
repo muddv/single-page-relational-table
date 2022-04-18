558c594f16f6c3115e4efd1a3a70996f0c472de0
misha <dedovmu@gmail.com>
2022-04-18 13:50:29 +0300

    Application is in working order

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from crypt import methods
 import secrets
 from flask import Flask, request, render_template
 import MySQLdb
@@ -14,9 +15,34 @@ conn = MySQLdb.connect(HOST, USERNAME, PASSWORD, "warehouse")
 cursor = conn.cursor()
 app = Flask(__name__)
 
-@app.route('/')
+CRITERIA = [
+    "shipment_date",
+    "item",
+    "amount",
+    "shipment_distance"
+]
+
+#maybe this can be passed to another conditional and names can be made more human readable
+CONDITIONS = [
+    "=",
+    ">",
+    "<",
+    "LIKE",
+]
+
+@app.route("/", methods=["GET", "POST"])
 def main():
-    cursor.execute("SELECT * from shipments")
+    column = request.form.get("column")
+    condition = request.form.get("condition")
+    query = request.form.get("query")
+    cursor.execute("SELECT * FROM shipments") 
+    if request.method == "POST":
+        if column == None or condition == None or query == None:
+            cursor.execute("SELECT * FROM shipments") 
+        else:
+            #sql = f"SELECT * FROM shipments WHERE {column} {condition} {query}"
+            #cursor.execute(sql)
+            cursor.execute("SELECT * FROM shipments WHERE %(column)s %(condition)s %(query)s", {'column': column, 'condition': condition, 'query': query});
     data = cursor.fetchall()
-    return render_template('index.html', value=data)
+    return render_template('index.html', data = data, criteria = CRITERIA, conditions = CONDITIONS)
 
